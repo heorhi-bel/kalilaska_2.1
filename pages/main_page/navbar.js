b0231987a9a770fe8c0b3a35f0ca96de66426d02
heorhi-bel <georgix@mail.ru>
2020-06-11 20:24:35 +0300

    a lot of changes and moral pain

---
+++ pages/main_page/navbar.js
@@ -0,0 +1,38 @@
+"use strict"
+/*
+let header = document.querySelector('header');
+
+header.addEventListener('click', function (event){
+    if(event.target.closest('span')){
+        let navbar = document.querySelector('.navbar');
+        // navbar.classList.toggle('navbar-open');
+        if(navbar.style.display == 'none'){
+            navbar.style.display = 'flex';
+            setInterval(function(){
+                let i = 0;
+                while( i < 401 ){
+                    navbar.style.width = i + 'px'
+                    i+=20;
+                }
+            }, 10)
+        } else{
+            let i = 400;
+            while( i < 1){
+                setInterval( () => navbar.style.width = i + 'px', 10)
+                i = i - 20;
+            }
+            navbar.style.display = 'none';
+        }
+
+    }
+});
+*/
+/*
+function clickToggle(event){
+    let optionIcon = document.querySelector('.option-icon')
+    // if(event.target == option 
+    alert(event.target)
+    let navbar = document.querySelector('.navbar');
+    navbar.classList.toggle('navbar-open');
+}
+*/
